f5600ffec2e6c11c35d20c421699617e5cb9ec9e
Maurício Damásio <mauriciodamasio123@gmail.com>
2025-04-09 08:21:15 +0100

    incluíndo rota das APPS

--- school_managments/urls.py
+++ school_managments/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    
+    path('', include('webpage.urls')),
+    path('usuarios/', include('accounts.urls')),
+    path('alunos/', include('alunos.urls')),
+    path('professores/', include('professores.urls')),
+    path('turmas/', include('turmas.urls')),
     path('admin/', admin.site.urls),
+
 ]
