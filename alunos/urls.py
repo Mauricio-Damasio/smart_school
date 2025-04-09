ab43bfaf140a43b9740682160bc0377ae94c9385
Maurício Damásio <mauriciodamasio123@gmail.com>
2025-04-09 11:13:07 +0100

    adicionando rotas

--- alunos/urls.py
+++ alunos/urls.py
@@ -1,6 +1,13 @@
-from django.contrib import admin
 from django.urls import path
+from .import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    
+    path('', views.AlunoIndexView, name= "index-alunuo"),
+    path('alunos/', views.alunosListView, name="alunos"),
+    path('alunos-efectivos/', views.alunosEfectivos, name="alunos-efectivos"),
+    path('aluno-novo/', views.alunoNewView, name="aluno-novo"),
+    path('<int:id>/deletar', views.deletarAluno, name="aluno-deletar"),
+    path('<int:id>/alterar', views.alterarAluno, name="alterar-aluno")
+    
 ]
--- alunos/views.py
+++ alunos/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# --- General views --- #
+
+def AlunoIndexView(request):
+  return render(request, 'alunos/index_aluno.html')
+
+def alunosListView(request):
+  return render(request, 'alunos/alunos.html')
+
+def alunosEfectivos(request):
+  return render(request,  'alunos/alunos_efectivos.html')
+
+def alunoNewView(request):
+  return render(request, 'alunos/aluno_novo.html')
+
+def deletarAluno(request,id):
+  return render(request, 'alunos/aluno_delete.html',{"id":id})
+
+def alterarAluno(request,id):
+  return render(request, 'alunos/aluno_alterar.html',  {"id": id})
